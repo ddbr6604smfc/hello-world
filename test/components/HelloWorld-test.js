1dd922ff20b3189098f3e3566923e5e294c38315
ddbr6604smfc <ddbr6604smfc@gmail.com>
2015-10-17 18:22:15 -0500

    rework tests

--- test/components/HelloWorld-test.js
+++ test/components/HelloWorld-test.js
@@ -1,12 +1,9 @@
 import expect from 'expect';
 import React from 'react';
 import TestUtils from 'react-addons-test-utils';
-import jsdomReact from '../helpers/jsdomReact';
 import HelloWorld from '../../src/components/HelloWorld';
 
 describe('HelloWorld component', () => {
-  jsdomReact();
-
   it('should render correctly', () => {
     const renderer = TestUtils.createRenderer();
     renderer.render(<HelloWorld />);
